67e88644ba403f31fdce8eb85f9a75e345d405c2
Isuri1 <isubabe1>
2021-04-22 16:27:38 +0800

    Share connections in connection pool by multiple clients

---
+++ src/test/java/com/zuhlke/apachecalcitedemo/AvaticaHttpServerTest.java
@@ -0,0 +1,22 @@
+package com.zuhlke.apachecalcitedemo;
+
+import com.zuhlke.apachecalcitedemo.src.main.java.AvaticaHttpServer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AvaticaHttpServerTest {
+
+    private AvaticaHttpServer avaticaHttpServer = new AvaticaHttpServer();
+
+    AvaticaHttpServerTest() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void testCustomImpersonationConfig() throws Exception {
+        avaticaHttpServer.createServer();
+    }
+}
